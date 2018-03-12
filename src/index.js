544d82dc0dad513a07889e2343dd8cd90fe054dd
oleg291291@mail.ru <oleg291291@mail.ru>
2018-03-12 13:06:50 +0300

    str

--- src/index.js
+++ src/index.js
@@ -7,11 +7,56 @@ for (let i = 0; i < pairs.length; i++){
   N = N * pow 
 }
 console.log(N)
+if (N > 999){
+  console.log("break!")
+  return "break!"
+}
 //////////////////////
 
+function gcd_rec(a, b) {
+    if (b) {
+        return gcd_rec(b, a % b);
+    } else {
+        return Math.abs(a);
+    }
+}
+
+/////////////////////
+var kArr = [];
+
+for(let j = 0; j < s.length; j++){
 
 
+  if(s[j] == '1'){
+    console.log('1!');
+    let kArrPre1 = "";
+    for(let k = 0; k <= N; k++){
+      let b = k + j
+        if (b != 0 && gcd_rec(N, b) == 1){
+            kArrPre1 += k + ",";
+        }
+    }
+    kArr.push(kArrPre1);
+    console.log(kArrPre1);
+  }
+  if(s[j] == '0') {
+    console.log('0!');
+    let kArrPre0 = "";
+    for(let kk = 0; kk <= N; kk++){
+      let c = kk + j
+        if (c != 0 && gcd_rec(N, c) != 1){
+            kArrPre0 += kk + ",";
+        }
+    }
+    
+    console.log(kArrPre0);
+    kArr.push(kArrPre0);
+    
+  }
+}
 
+
+console.log(kArr);
 
 
 }
