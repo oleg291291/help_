10f64582576dd8679252415b99203f8f788d0c5c
oleg291291@mail.ru <oleg291291@mail.ru>
2018-03-12 11:43:17 +0300

    N

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function count(s, pairs) {
   // your implementation
+
+var N = 1;
+for (let i = 0; i < pairs.length; i++){
+  var pow = (pairs[i][0] ** pairs[i][1]) 
+  N = N * pow 
+}
+console.log(N)
+//////////////////////
+
+
+
+
+
+
 }
